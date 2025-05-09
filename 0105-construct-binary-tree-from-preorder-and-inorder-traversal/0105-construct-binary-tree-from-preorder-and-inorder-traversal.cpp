9818d738cc9d08cdac564c22358b90779ec9d88d
Hritvik mohan <126161202+hritvik715@users.noreply.github.com>
2025-05-09 10:30:28 +0530

    Time: 0 ms (100%), Space: 27.8 MB (17.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,9 +9,69 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+ 
+//     int getIndexInorder(vector<int> inorder, int target) {
+//         for(int i=0; i<inorder.size(); i++) {
+//             if(inorder[i] == target) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+
+
+//     //If we passed preOrderIndex by value, each recursive call would get a copy, and the changes (like preOrderIndex++) wouldn't affect other calls.
+//     //By passing it by reference, all recursive calls share and update the same index, progressing through preorder correctly.
+//     TreeNode* constructTree( vector<int>& preorder, vector<int>& inorder, int &preOrderIndex, int inorderStart, 
+//     int inorderEnd, int size) {
+//         //base case
+//         if(preOrderIndex >= size) {
+//             return NULL;
+//         }
+//         if(inorderStart > inorderEnd) {
+//             return NULL;
+//         }
+
+//         //process the first element of preorder as root
+//         //step1: get element from preorder and create root node for it 
+//         int element = preorder[preOrderIndex];
+//         preOrderIndex++;
+//         TreeNode* root = new TreeNode(element);
+//         //step2: find this element inside inorder and call for left and rigth 
+//         int elementIndexInsideInorder = getIndexInorder(inorder,element);
+//         //inorder left part -> index inorderStart to elementIndexInsideInorder-1
+//         //inorder right part -> index elementIndexInsideInorder+1 to inorderEnd
+//         root->left = constructTree(preorder, inorder, preOrderIndex, inorderStart, elementIndexInsideInorder-1,size);
+//         root->right = constructTree( preorder, inorder, preOrderIndex, elementIndexInsideInorder+1 , inorderEnd, size);
+//         return root;
+//     }
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//         int preIndex = 0;
+//         int inorderStart = 0;
+//         int inorderEnd = inorder.size()-1;
+//         int size = inorder.size();
+        
+        
+//         TreeNode* root = constructTree(preorder, inorder, preIndex, inorderStart, inorderEnd, size);
+//         return root;
+//     }
+
+// };
+
+
+
+//optimal way
 class Solution {
 public:
- 
+    void createMapping(unordered_map<int,int> &valueToIndex,vector<int> inorder) {
+        for(int i=0; i<inorder.size(); i++) {
+            int element = inorder[i];
+            int index = i;
+            valueToIndex[element] = index;
+        }
+    }
     int getIndexInorder(vector<int> inorder, int target) {
         for(int i=0; i<inorder.size(); i++) {
             if(inorder[i] == target) {
@@ -20,11 +80,7 @@ public:
         }
         return -1;
     }
-
-
-    //If we passed preOrderIndex by value, each recursive call would get a copy, and the changes (like preOrderIndex++) wouldn't affect other calls.
-    //By passing it by reference, all recursive calls share and update the same index, progressing through preorder correctly.
-    TreeNode* constructTree( vector<int>& preorder, vector<int>& inorder, int &preOrderIndex, int inorderStart, 
+    TreeNode* constructTree(unordered_map<int,int> &valMap, vector<int>& preorder, vector<int>& inorder, int &preOrderIndex, int inorderStart, 
     int inorderEnd, int size) {
         //base case
         if(preOrderIndex >= size) {
@@ -40,11 +96,11 @@ public:
         preOrderIndex++;
         TreeNode* root = new TreeNode(element);
         //step2: find this element inside inorder and call for left and rigth 
-        int elementIndexInsideInorder = getIndexInorder(inorder,element);
+        int elementIndexInsideInorder = valMap[element];
         //inorder left part -> index inorderStart to elementIndexInsideInorder-1
         //inorder right part -> index elementIndexInsideInorder+1 to inorderEnd
-        root->left = constructTree(preorder, inorder, preOrderIndex, inorderStart, elementIndexInsideInorder-1,size);
-        root->right = constructTree( preorder, inorder, preOrderIndex, elementIndexInsideInorder+1 , inorderEnd, size);
+        root->left = constructTree(valMap,preorder, inorder, preOrderIndex, inorderStart, elementIndexInsideInorder-1,size);
+        root->right = constructTree(valMap, preorder, inorder, preOrderIndex, elementIndexInsideInorder+1 , inorderEnd, size);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
@@ -52,10 +108,20 @@ public:
         int inorderStart = 0;
         int inorderEnd = inorder.size()-1;
         int size = inorder.size();
-        
-        
-        TreeNode* root = constructTree(preorder, inorder, preIndex, inorderStart, inorderEnd, size);
+        unordered_map<int,int> valMap;
+        createMapping(valMap, inorder);
+        TreeNode* root = constructTree(valMap,preorder, inorder, preIndex, inorderStart, inorderEnd, size);
         return root;
     }
+
+
+
+
+
+
+
+
+
+
 
 };
