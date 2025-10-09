52c01164a8a411666380f45da0f2e9a129469d46
Hritvik mohan <126161202+hritvik715@users.noreply.github.com>
2025-10-10 02:49:24 +0530

    Time: 164 ms (5.03%), Space: 89.5 MB (8.93%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+          map<int, int> freq;  // map to store frequency of each number
+
+    for (int num : nums) {
+        freq[num]++;              // increase count
+        if (freq[num] > 1) {      // if count > 1 → duplicate found
+            return num;
+        }
+    }
+
+    return -1; // no duplicate found (theoretically won't happen as per problem)
+    }
+};
