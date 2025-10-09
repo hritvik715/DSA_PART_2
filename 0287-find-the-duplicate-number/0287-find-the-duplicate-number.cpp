ce536e22adf7208bb3f81a2503d6b92475f3f680
Hritvik mohan <126161202+hritvik715@users.noreply.github.com>
2025-10-10 03:07:18 +0530

    Time: 167 ms (5.03%), Space: 89.3 MB (10.32%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,7 +4,8 @@ public:
         map<int, int> freq;  // map to store frequency of each number
 
         for (int num : nums) {
-            freq[num]++;              // increase count
+            // freq[num]++;              // increase count
+            freq[num] = freq[num] + 1; //freq[1] does not exist yet, so by default freq[1] = 0
             if (freq[num] > 1) {      // if count > 1 → duplicate found
                 return num;
             }
@@ -13,3 +14,14 @@ public:
         return -1; // no duplicate found (theoretically won't happen as per problem)
     }
 };
+
+
+//  Iteration 1 → num = 3
+//  freq = { 3 : 1 }
+
+//  Iteration 2 → num = 1
+//  freq = { 1 : 1 , 3 : 1 }
+
+//  Iteration 3 → num = 3
+//  freq = { 1 : 1 , 3 : 2 }  ← duplicate found here!
+//  return 3
