3a4ec23375e126c6cc195ce8784f67664b4b0a18
Hritvik mohan <126161202+hritvik715@users.noreply.github.com>
2025-10-13 00:26:42 +0530

    Time: 23 ms (5.92%), Space: 24.4 MB (5.55%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,73 +1,18 @@
 class Solution {
 public:
-
-    void printMap(unordered_map<int , int>&freqMap){
-        unordered_map<int , int>::iterator it;
-        cout<<"\n---Map has below things-------\n";
-           for(it = freqMap.begin();it!=freqMap.end();it++){
-            int key = it->first;
-            int  freq = it->second;
-            cout<<key << " " <<freq<<endl;
-            
-        }
-    }
-
     int singleNumber(vector<int>& nums) {
-        // int ans = 0;
-        // int n = nums.size();
-        // for (int i = 0; i < n; i++) {
-        //     ans = ans ^ nums[i];
-        // }
-        // return ans;
+        map<int,int> freq;
 
-
-        //2nd way
-        // sort(nums.begin() ,nums.end());// vector increasing order
-        // int n = nums.size();
-        // int ans = -1;
-        // int i =0;
-        // while(i < n){
-        //     if( i + 1 < n && nums[i] == nums[i+1]){
-        //          i = i+2;
-        //     }
-        //     else{
-        //         ans = nums[i];
-        //         break;
-        //     }
-
-        // }
-        // return ans;
-    
-    
-    
-
-
-
-        //3rd way (hashing method)
-        //frequncy table
-        unordered_map<int , int>freqMap;
-        
-        //add freq to the map/table
-        for(int i =0 ; i<nums.size() ;i++){
-            int num = nums[i];
-            freqMap[num]=freqMap[num] +1;//suru may MAP mey entry hai hi nhi toh vo apne aap zero de dega initially
-            // printMap(freqMap);
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
         }
-        // return -1;
 
-        unordered_map<int , int>::iterator it;
-        int ans;
-        for(it = freqMap.begin();it!=freqMap.end();it++){
-            int key = it->first;
-            int  freq = it->second;
-            // cout<<key << " " <<freq<<endl;
-            if(freq == 1){
-                ans = key;
-                break;
+
+        for(int i=0;i<nums.size();i++){
+            if(freq[nums[i]] == 1){
+                return nums[i];
             }
-            
         }
-     
-        return ans; 
+        return 0;
     }
 };
